5e100de9b1697201d5f6042433e12f0db7ce9523
Mark Agne <magnenator@gmail.com>
2019-11-06 20:07:46 -0600

    new mock eniromnent for update system tests, refactored existing test to use it

--- Tests/Runtime/Scripts/UpdateSystemTests.cs
+++ Tests/Runtime/Scripts/UpdateSystemTests.cs
@@ -1,32 +1,99 @@
 ﻿using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine.TestTools;
 
 namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 {
-    public sealed class UpdateSystemTests
-    {
-        [Test]
-        public void Instantiate_BasicManagedUpdateActive_AddedToSystem()
-        {
-			var updateManager = ScriptableObject.CreateInstance<CustomUpdateManager>();
-			var defaultPriorityGroup = ScriptableObject.CreateInstance<CustomUpdatePriority>();
+	public sealed class UpdateSystemTests
+	{
+		[System.Flags]
+		private enum MockObjectToggleConfig : int
+		{
+			None = 0,
+			GameObjectActive = 1,
+			ComponentEnabled = 2,
+			Update = 4,
+			FixedUpdate = 8,
 
-			updateManager.Initialize(defaultPriorityGroup);
+			AllActiveAndEnabled = GameObjectActive | ComponentEnabled | Update | FixedUpdate,
+			UpdateActiveAndEnabled = AllActiveAndEnabled & ~FixedUpdate,
+			FixedUpdateActiveAndEnabled = AllActiveAndEnabled & ~Update,
+			NoUpdatesActiveAndEnabled = GameObjectActive | ComponentEnabled
+		}
 
-			var component = CustomUpdateBehaviour.Create<TestBasicManagedUpdatesComponent>(
-				new CustomUpdateManager.Config(updateManager, defaultPriorityGroup, true, false),
-				true,
-				true
-			);
+		private sealed class MockEnvironment : System.IDisposable
+		{
+			public readonly CustomUpdateManager manager;
+			public readonly ReadOnlyDictionary<string, CustomUpdatePriority> priorities;
 
-			Assert.That(
-				updateManager.GetCountForGroup(defaultPriorityGroup, CustomUpdateManager.UpdateType.Normal) == 1,
-				$"There should be exactly one {nameof(CustomUpdateBehaviour)} with UPDATE running in the system."
-			);
+			public MockEnvironment(params string[] priorityGroupNames)
+			{
+				int groupCount = priorityGroupNames.Length;
 
-			Object.Destroy(updateManager);
-			Object.Destroy(defaultPriorityGroup);
-        }
-    }
+				if (groupCount == 0)
+				{
+					throw new System.ArgumentException("Mock update system enviromnent must have at least one priority group.");
+				}
+
+				var priorityGroups = new CustomUpdatePriority[groupCount];
+				var priorityLookup = new Dictionary<string, CustomUpdatePriority>(groupCount);
+				for (int i = 0; i < groupCount; i++)
+				{
+					var currentGroup = priorityGroups[i] = ScriptableObject.CreateInstance<CustomUpdatePriority>();
+					priorityLookup.Add(priorityGroupNames[i], currentGroup);
+				}
+				priorities = new ReadOnlyDictionary<string, CustomUpdatePriority>(priorityLookup);
+
+				manager = ScriptableObject.CreateInstance<CustomUpdateManager>();
+				manager.Initialize(priorityGroups);
+			}
+
+			public void InstantiateManagedComponents<T>(string groupName, params MockObjectToggleConfig[] toggleConfig) where T : CustomUpdateBehaviour
+			{
+				var group = priorities[groupName];
+				foreach (var config in toggleConfig)
+				{
+					CustomUpdateBehaviour.Create<T>(
+						new CustomUpdateManager.Config(
+							manager,
+							group,
+							config.HasFlag(MockObjectToggleConfig.Update),
+							config.HasFlag(MockObjectToggleConfig.FixedUpdate)),
+						config.HasFlag(MockObjectToggleConfig.GameObjectActive),
+						config.HasFlag(MockObjectToggleConfig.ComponentEnabled)
+					);
+				}
+			}
+
+			public void Dispose()
+			{
+				Object.Destroy(manager);
+
+				foreach (var kvp in priorities)
+				{
+					Object.Destroy(kvp.Value);
+				}
+			}
+		}
+
+		[Test]
+		public void Instantiate_BasicManagedUpdateActive_AddedToSystem()
+		{
+			string groupName = "Default";
+			using (MockEnvironment env = new MockEnvironment(groupName))
+			{
+				env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
+					groupName,
+					MockObjectToggleConfig.UpdateActiveAndEnabled
+				);
+
+				Assert.That(
+					env.manager.GetCountForGroup(env.priorities[groupName], CustomUpdateManager.UpdateType.Normal) == 1 &&
+					env.manager.GetCountForGroup(env.priorities[groupName], CustomUpdateManager.UpdateType.Fixed) == 0,
+					$"There should be exactly one {nameof(CustomUpdateBehaviour)} with UPDATE running in the system."
+				);
+			}
+		}
 }
