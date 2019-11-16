12d771da791106352a47ce8777f62b84cfc4f54f
Mark Agne <magnenator@gmail.com>
2019-11-16 14:09:03 -0600

    added test component for MonoBehaviour Update/FixedUpdate

---
+++ Tests/Runtime/Scripts/TestBasicUnityUpdatesComponent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Miscreant.Utilities.Lifecycle.RuntimeTests
+{
+	public sealed class TestBasicUnityUpdatesComponent : MonoBehaviour
+	{
+		private void Update()
+		{
+			// Do nothing
+		}
+
+		private void FixedUpdate()
+		{
+			// Do nothing
+		}
+	}
+}
