ad766cec166e5c19dc608b93f456872b7879e24c
Mark Agne <magnenator@gmail.com>
2019-12-13 02:26:49 -0600

    added set of exceptions to use for update system validation errors

--- Runtime/Scripts/CustomUpdateManager.cs
+++ Runtime/Scripts/CustomUpdateManager.cs
@@ -666,5 +666,70 @@ namespace Miscreant.Utilities.Lifecycle
 				_fixedUpdateLists[i].ExecuteAll();
 			}
 		}
+		#region Exceptions
+
+		//
+		// General
+		//
+
+		public class InvalidUpdateTypeException : Exception
+		{
+			public InvalidUpdateTypeException(UpdateType type) : base($"{nameof(UpdateType)} cannot be specified as {type}.") { }
+		}
+
+		//
+		// List State
+		//
+
+		public class DuplicateReferenceException : Exception
+		{
+			public DuplicateReferenceException() : base("Linked list contains more than one reference of the same component.") { }
+		}
+
+		public class EmptyGroupWithNonNullHeadOrTailException : Exception
+		{
+			public EmptyGroupWithNonNullHeadOrTailException() : base("For an empty group, both head and tail must be null.") { }
+		}
+
+		public class NonemptyGroupWithNullHeadOrTailException : Exception
+		{
+			public NonemptyGroupWithNullHeadOrTailException() : base("For a non-empty group, neither head nor tail can be null.") { }
+		}
+
+		public class SingleElementGroupWithDifferentHeadAndTailException : Exception
+		{
+			public SingleElementGroupWithDifferentHeadAndTailException() : base("For a single-item group, head and tail must be the same component.") { }
+		}
+
+		public class InvalidTailLinkException : Exception
+		{
+			public InvalidTailLinkException() : base("Tail component's link must be set to null.") { }
+		}
+
+		public class TailMismatchException : Exception
+		{
+			public TailMismatchException() : base("Final link in the group does not match the expected tail.") { }
+		}
+
+		public class InvalidCountException : Exception
+		{
+			public InvalidCountException() : base("Total number of components in group does not match the expected count.") { }
+		}
+
+		//
+		// Component State
+		//
+
+		public class PriorityGroupMismatchException : Exception
+		{
+			public PriorityGroupMismatchException() : base("Element's priority group does not match the expected group.") { }
+		}
+
+		public class ManagerMismatchException : Exception
+		{
+			public ManagerMismatchException() : base("Assigned manager does not match the current system.") { }
+		}
+
+		#endregion
 	}
 }
