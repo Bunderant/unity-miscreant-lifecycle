35ba76b795d1bf97c4e9072703472ee49b9e3784
Mark Agne <magnenator@gmail.com>
2019-12-12 21:13:25 -0600

    beginning of toggle state permutation testing class (method to generate permutations array of Flags enums)

---
+++ Tests/Runtime/Scripts/UpdateSystemTests_ToggleState.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using NUnit.Framework;
+
+namespace Miscreant.Utilities.Lifecycle.RuntimeTests
+{
+	using ObjectToggleConfig = FakeEnvironment.ObjectToggleConfig;
+
+	public class UpdateSystemTests_ToggleState
+	{
+		/// <summary>
+		/// Generates all permutations of a 'flags' Enum type, filtering out named combinations of flags.
+		/// </summary>
+		public static T[] GenerateAllTogglePermutations<T>() where T : Enum
+		{
+			if (!typeof(T).IsDefined(typeof(FlagsAttribute), false))
+			{
+				throw new ArgumentException($"{typeof(FlagsAttribute).Name} must be defined to generate the permutations of the given Enum: {typeof(T).Name}");
+			}
+
+			var allNamedConfigValues = System.Enum.GetValues(typeof(ObjectToggleConfig)) as ObjectToggleConfig[];
+			int allToggledOnValue = 0;
+			foreach (ObjectToggleConfig toggleConfig in allNamedConfigValues)
+			{
+				int toggleConfigValue = (int)toggleConfig;
+				double exponent = System.Math.Log(toggleConfigValue, 2);
+
+				// If the base 2 log of the config value is an integer, we know the value represents a single flag and not a named combination
+				bool isInteger = System.Math.Ceiling(exponent) == System.Math.Floor(exponent);
+				if (isInteger)
+				{
+					allToggledOnValue |= toggleConfigValue;
+				}
+			}
+
+			var permutations = new SortedSet<T>();
+			for (int currentValue = 0; currentValue <= allToggledOnValue; currentValue++)
+			{
+				if ((allToggledOnValue | currentValue) == allToggledOnValue)
+				{
+					permutations.Add((T)(ValueType)currentValue);
+				}
+			}
+
+			var permutationsArray = new T[permutations.Count];
+			permutations.CopyTo(permutationsArray);
+
+			return permutationsArray;
+		}
+	}
+}
+
