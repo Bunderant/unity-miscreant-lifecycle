741fba10771ac139b1929034763ce3958ee0651a
Mark Agne <magnenator@gmail.com>
2019-12-12 20:24:25 -0600

    moved "MockEnvironment" out to its own file, renamed to "FakeEnvironment"

---
+++ Tests/Runtime/Scripts/FakeEnvironment.cs
@@ -0,0 +1,178 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Object = UnityEngine.Object;
+
+namespace Miscreant.Utilities.Lifecycle.RuntimeTests
+{
+	using UpdateType = CustomUpdateManager.UpdateType;
+
+	public sealed class FakeEnvironment : IDisposable
+	{
+		[Flags]
+		public enum ObjectToggleConfig : int
+		{
+			None = 0,
+			GameObjectActive = 1,
+			ComponentEnabled = 2,
+			Update = 4,
+			FixedUpdate = 8,
+
+			AllActiveAndEnabled = GameObjectActive | ComponentEnabled | Update | FixedUpdate,
+			UpdateActiveAndEnabled = AllActiveAndEnabled & ~FixedUpdate,
+			FixedUpdateActiveAndEnabled = AllActiveAndEnabled & ~Update,
+			NoUpdatesActiveAndEnabled = GameObjectActive | ComponentEnabled
+		}
+
+		public readonly CustomUpdateManager manager;
+		public readonly ReadOnlyDictionary<string, CustomUpdatePriority> priorities;
+		private readonly TestManagedUpdatesSceneController _runtimeController;
+
+		public FakeEnvironment(params string[] priorityGroupNames)
+		{
+			int groupCount = priorityGroupNames.Length;
+
+			if (groupCount == 0)
+			{
+				throw new System.ArgumentException("Fake environment must have at least one priority group.");
+			}
+
+			var priorityGroups = new CustomUpdatePriority[groupCount];
+			var priorityLookup = new Dictionary<string, CustomUpdatePriority>(groupCount);
+			for (int i = 0; i < groupCount; i++)
+			{
+				var currentGroup = priorityGroups[i] = ScriptableObject.CreateInstance<CustomUpdatePriority>();
+				priorityLookup.Add(priorityGroupNames[i], currentGroup);
+			}
+			priorities = new ReadOnlyDictionary<string, CustomUpdatePriority>(priorityLookup);
+
+			manager = ScriptableObject.CreateInstance<CustomUpdateManager>();
+			manager.Initialize(priorityGroups);
+
+			_runtimeController = new GameObject("Runtime Controller").AddComponent<TestManagedUpdatesSceneController>();
+			_runtimeController.enabled = false;
+		}
+
+		public static void GetExpectedDeltaForComponentToggleChange(
+			ObjectToggleConfig before,
+			ObjectToggleConfig after,
+			out int updateDelta,
+			out int fixedUpdateDelta)
+		{
+			updateDelta = 0;
+			if (before.HasFlag(ObjectToggleConfig.UpdateActiveAndEnabled) &&
+				!after.HasFlag(ObjectToggleConfig.UpdateActiveAndEnabled))
+			{
+				updateDelta = -1;
+			}
+			else if (!before.HasFlag(ObjectToggleConfig.UpdateActiveAndEnabled) &&
+				after.HasFlag(ObjectToggleConfig.UpdateActiveAndEnabled))
+			{
+				updateDelta = 1;
+			}
+
+			fixedUpdateDelta = 0;
+			if (before.HasFlag(ObjectToggleConfig.FixedUpdateActiveAndEnabled) &&
+				!after.HasFlag(ObjectToggleConfig.FixedUpdateActiveAndEnabled))
+			{
+				fixedUpdateDelta = -1;
+			}
+			else if (!before.HasFlag(ObjectToggleConfig.FixedUpdateActiveAndEnabled) &&
+				after.HasFlag(ObjectToggleConfig.FixedUpdateActiveAndEnabled))
+			{
+				fixedUpdateDelta = 1;
+			}
+		}
+
+		public void InstantiateManagedComponents<T>(string groupName, params ObjectToggleConfig[] toggleConfig) where T : CustomUpdateBehaviour
+		{
+			CustomUpdatePriority group = priorities[groupName];
+			Transform containerTransform = _runtimeController.transform;
+
+			foreach (ObjectToggleConfig config in toggleConfig)
+			{
+				InstantiateManagedUpdateGameObject<T>(group, config, containerTransform);
+			}
+		}
+
+		public void InstantiateManagedComponents<T>(
+			out T[] components,
+			string groupName,
+			params ObjectToggleConfig[] toggleConfig) where T : CustomUpdateBehaviour
+		{
+			CustomUpdatePriority group = priorities[groupName];
+			Transform containerTransform = _runtimeController.transform;
+
+			components = new T[toggleConfig.Length];
+			for (int i = 0; i < toggleConfig.Length; i++)
+			{
+				components[i] = InstantiateManagedUpdateGameObject<T>(group, toggleConfig[i], containerTransform);
+			}
+		}
+
+		public int GetCountForGroup(string groupName, UpdateType updateType)
+		{
+			return manager.GetCountForGroup(priorities[groupName], updateType);
+		}
+
+		private T InstantiateManagedUpdateGameObject<T>(
+			CustomUpdatePriority group,
+			ObjectToggleConfig config,
+			Transform parent = null) where T : CustomUpdateBehaviour
+		{
+			return CustomUpdateBehaviour.Create<T>(
+				new CustomUpdateManager.Config(
+					manager,
+					group,
+					config.HasFlag(ObjectToggleConfig.Update),
+					config.HasFlag(ObjectToggleConfig.FixedUpdate)),
+				config.HasFlag(ObjectToggleConfig.GameObjectActive),
+				config.HasFlag(ObjectToggleConfig.ComponentEnabled),
+				parent
+			);
+		}
+
+		public void SetToggleConfigForManagedComponent(CustomUpdateBehaviour component, ObjectToggleConfig toggleConfig)
+		{
+			component.gameObject.SetActive(toggleConfig.HasFlag(ObjectToggleConfig.GameObjectActive));
+			component.enabled = toggleConfig.HasFlag(ObjectToggleConfig.ComponentEnabled);
+			component.updateConfig.update = toggleConfig.HasFlag(ObjectToggleConfig.Update);
+			component.updateConfig.fixedUpdate = toggleConfig.HasFlag(ObjectToggleConfig.FixedUpdate);
+		}
+
+		public void StartUpdating()
+		{
+			_runtimeController.OnMonoBehaviourUpdate = (() =>
+			{
+				manager.RunUpdate();
+			});
+
+			_runtimeController.OnMonoBehaviourFixedUpdate = (() =>
+			{
+				manager.RunFixedUpdate();
+			});
+
+			_runtimeController.enabled = true;
+		}
+
+		public void StopUpdating()
+		{
+			_runtimeController.enabled = false;
+			_runtimeController.OnMonoBehaviourUpdate = null;
+			_runtimeController.OnMonoBehaviourFixedUpdate = null;
+		}
+
+		public void Dispose()
+		{
+			Object.Destroy(_runtimeController.gameObject);
+			Object.Destroy(manager);
+
+			foreach (var kvp in priorities)
+			{
+				Object.Destroy(kvp.Value);
+			}
+		}
+	}
+}
--- Tests/Runtime/Scripts/UpdateSystemTests.cs
+++ Tests/Runtime/Scripts/UpdateSystemTests.cs
@@ -1,13 +1,12 @@
 ﻿using NUnit.Framework;
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using UnityEngine.TestTools;
 
 namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 {
 	using UpdateType = CustomUpdateManager.UpdateType;
+	using ObjectToggleConfig = FakeEnvironment.ObjectToggleConfig;
 
 	public sealed class UpdateSystemTests
 	{
@@ -29,184 +28,17 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 			public override string ToString() => $"{value}";
 		}
 
-		[System.Flags]
-		private enum MockObjectToggleConfig : int
-		{
-			None = 0,
-			GameObjectActive = 1,
-			ComponentEnabled = 2,
-			Update = 4,
-			FixedUpdate = 8,
-
-			AllActiveAndEnabled = GameObjectActive | ComponentEnabled | Update | FixedUpdate,
-			UpdateActiveAndEnabled = AllActiveAndEnabled & ~FixedUpdate,
-			FixedUpdateActiveAndEnabled = AllActiveAndEnabled & ~Update,
-			NoUpdatesActiveAndEnabled = GameObjectActive | ComponentEnabled
-		}
-
-		private sealed class MockEnvironment : System.IDisposable
-		{
-			public readonly CustomUpdateManager manager;
-			public readonly ReadOnlyDictionary<string, CustomUpdatePriority> priorities;
-			private readonly TestManagedUpdatesSceneController _runtimeController;
-
-			public MockEnvironment(params string[] priorityGroupNames)
-			{
-				int groupCount = priorityGroupNames.Length;
-
-				if (groupCount == 0)
-				{
-					throw new System.ArgumentException("Mock update system enviromnent must have at least one priority group.");
-				}
-
-				var priorityGroups = new CustomUpdatePriority[groupCount];
-				var priorityLookup = new Dictionary<string, CustomUpdatePriority>(groupCount);
-				for (int i = 0; i < groupCount; i++)
-				{
-					var currentGroup = priorityGroups[i] = ScriptableObject.CreateInstance<CustomUpdatePriority>();
-					priorityLookup.Add(priorityGroupNames[i], currentGroup);
-				}
-				priorities = new ReadOnlyDictionary<string, CustomUpdatePriority>(priorityLookup);
-
-				manager = ScriptableObject.CreateInstance<CustomUpdateManager>();
-				manager.Initialize(priorityGroups);
-
-				_runtimeController = new GameObject("Runtime Controller").AddComponent<TestManagedUpdatesSceneController>();
-				_runtimeController.enabled = false;
-			}
-
-			public static void GetExpectedDeltaForComponentToggleChange(
-				MockObjectToggleConfig before,
-				MockObjectToggleConfig after,
-				out int updateDelta,
-				out int fixedUpdateDelta)
-			{
-				updateDelta = 0;
-				if (before.HasFlag(MockObjectToggleConfig.UpdateActiveAndEnabled) &&
-					!after.HasFlag(MockObjectToggleConfig.UpdateActiveAndEnabled))
-				{
-					updateDelta = -1;
-				}
-				else if (!before.HasFlag(MockObjectToggleConfig.UpdateActiveAndEnabled) &&
-					after.HasFlag(MockObjectToggleConfig.UpdateActiveAndEnabled))
-				{
-					updateDelta = 1;
-				}
-
-				fixedUpdateDelta = 0;
-				if (before.HasFlag(MockObjectToggleConfig.FixedUpdateActiveAndEnabled) &&
-					!after.HasFlag(MockObjectToggleConfig.FixedUpdateActiveAndEnabled))
-				{
-					fixedUpdateDelta = -1;
-				}
-				else if (!before.HasFlag(MockObjectToggleConfig.FixedUpdateActiveAndEnabled) &&
-					after.HasFlag(MockObjectToggleConfig.FixedUpdateActiveAndEnabled))
-				{
-					fixedUpdateDelta = 1;
-				}
-			}
-
-			public void InstantiateManagedComponents<T>(string groupName, params MockObjectToggleConfig[] toggleConfig) where T : CustomUpdateBehaviour
-			{
-				CustomUpdatePriority group = priorities[groupName];
-				Transform containerTransform = _runtimeController.transform;
-
-				foreach (MockObjectToggleConfig config in toggleConfig)
-				{
-					InstantiateManagedUpdateGameObject<T>(group, config, containerTransform);
-				}
-			}
-
-			public void InstantiateManagedComponents<T>(
-				out T[] components,
-				string groupName,
-				params MockObjectToggleConfig[] toggleConfig) where T : CustomUpdateBehaviour
-			{
-				CustomUpdatePriority group = priorities[groupName];
-				Transform containerTransform = _runtimeController.transform;
-
-				components = new T[toggleConfig.Length];
-				for (int i = 0; i < toggleConfig.Length; i++)
-				{
-					components[i] = InstantiateManagedUpdateGameObject<T>(group, toggleConfig[i], containerTransform);
-				}
-			}
-
-			public int GetCountForGroup(string groupName, UpdateType updateType)
-			{
-				return manager.GetCountForGroup(priorities[groupName], updateType);
-			}
-
-			private T InstantiateManagedUpdateGameObject<T>(
-				CustomUpdatePriority group,
-				MockObjectToggleConfig config,
-				Transform parent = null) where T : CustomUpdateBehaviour
-			{
-				return CustomUpdateBehaviour.Create<T>(
-					new CustomUpdateManager.Config(
-						manager,
-						group,
-						config.HasFlag(MockObjectToggleConfig.Update),
-						config.HasFlag(MockObjectToggleConfig.FixedUpdate)),
-					config.HasFlag(MockObjectToggleConfig.GameObjectActive),
-					config.HasFlag(MockObjectToggleConfig.ComponentEnabled),
-					parent
-				);
-			}
-
-			public void SetToggleConfigForManagedComponent(CustomUpdateBehaviour component, MockObjectToggleConfig toggleConfig)
-			{
-				component.gameObject.SetActive(toggleConfig.HasFlag(MockObjectToggleConfig.GameObjectActive));
-				component.enabled = toggleConfig.HasFlag(MockObjectToggleConfig.ComponentEnabled);
-				component.updateConfig.update = toggleConfig.HasFlag(MockObjectToggleConfig.Update);
-				component.updateConfig.fixedUpdate = toggleConfig.HasFlag(MockObjectToggleConfig.FixedUpdate);
-			}
-
-			public void StartUpdating()
-			{
-				_runtimeController.OnMonoBehaviourUpdate = (() =>
-				{
-					manager.RunUpdate();
-				});
-
-				_runtimeController.OnMonoBehaviourFixedUpdate = (() =>
-				{
-					manager.RunFixedUpdate();
-				});
-
-				_runtimeController.enabled = true;
-			}
-
-			public void StopUpdating()
-			{
-				_runtimeController.enabled = false;
-				_runtimeController.OnMonoBehaviourUpdate = null;
-				_runtimeController.OnMonoBehaviourFixedUpdate = null;
-			}
-
-			public void Dispose()
-			{
-				Object.Destroy(_runtimeController.gameObject);
-				Object.Destroy(manager);
-
-				foreach (var kvp in priorities)
-				{
-					Object.Destroy(kvp.Value);
-				}
-			}
-		}
-
 		[Test]
 		public void Instantiate_OneBasicManagedUpdateActive_AddedToEmptySystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(1),
 					new ExpectedFixedUpdateCount(0),
-					MockObjectToggleConfig.UpdateActiveAndEnabled
+					ObjectToggleConfig.UpdateActiveAndEnabled
 				);
 			}
 		}
@@ -214,14 +46,14 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void Instantiate_OneBasicManagedFixedUpdateActive_AddedToEmptySystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(0),
 					new ExpectedFixedUpdateCount(1),
-					MockObjectToggleConfig.FixedUpdateActiveAndEnabled
+					ObjectToggleConfig.FixedUpdateActiveAndEnabled
 				);
 			}
 		}
@@ -229,14 +61,14 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void Instantiate_OneBasicManagedUpdateAndFixedActive_AddedToEmptySystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(1),
 					new ExpectedFixedUpdateCount(1),
-					MockObjectToggleConfig.AllActiveAndEnabled
+					ObjectToggleConfig.AllActiveAndEnabled
 				);
 			}
 		}
@@ -244,18 +76,18 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void Instantiate_OneBasicManagedUpdateActive_AddedToPopulatedSystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
 					DEFAULT_GROUP_NAME,
-					MockObjectToggleConfig.UpdateActiveAndEnabled);
+					ObjectToggleConfig.UpdateActiveAndEnabled);
 
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(2),
 					new ExpectedFixedUpdateCount(0),
-					MockObjectToggleConfig.UpdateActiveAndEnabled
+					ObjectToggleConfig.UpdateActiveAndEnabled
 				);
 			}
 		}
@@ -263,18 +95,18 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void Instantiate_OneBasicManagedFixedUpdateActive_AddedToPopulatedSystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
 					DEFAULT_GROUP_NAME,
-					MockObjectToggleConfig.FixedUpdateActiveAndEnabled);
+					ObjectToggleConfig.FixedUpdateActiveAndEnabled);
 
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(0),
 					new ExpectedFixedUpdateCount(2),
-					MockObjectToggleConfig.FixedUpdateActiveAndEnabled
+					ObjectToggleConfig.FixedUpdateActiveAndEnabled
 				);
 			}
 		}
@@ -282,28 +114,28 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void Instantiate_OneBasicManagedUpdateAndFixedActive_AddedToPopulatedSystem()
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
 					DEFAULT_GROUP_NAME,
-					MockObjectToggleConfig.AllActiveAndEnabled);
+					ObjectToggleConfig.AllActiveAndEnabled);
 
 				RunObjectInstantiateCountValidationTest(
 					env,
 					DEFAULT_GROUP_NAME,
 					new ExpectedUpdateCount(2),
 					new ExpectedFixedUpdateCount(2),
-					MockObjectToggleConfig.AllActiveAndEnabled
+					ObjectToggleConfig.AllActiveAndEnabled
 				);
 			}
 		}
 
 		private static void RunObjectInstantiateCountValidationTest(
-			MockEnvironment env,
+			FakeEnvironment env,
 			string groupName,
 			ExpectedUpdateCount expectedUpdateCountAfter,
 			ExpectedFixedUpdateCount expectedFixedCountAfter,
-			params MockObjectToggleConfig[] instantiatedObjectConfigSettings)
+			params ObjectToggleConfig[] instantiatedObjectConfigSettings)
 		{
 			env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
 				groupName,
@@ -316,8 +148,8 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void EnableComponent_OneBasicManagedUpdate_AddsToStstem()
 		{
-			var before = MockObjectToggleConfig.GameObjectActive | MockObjectToggleConfig.Update;
-			var after = before | MockObjectToggleConfig.ComponentEnabled;
+			var before = ObjectToggleConfig.GameObjectActive | ObjectToggleConfig.Update;
+			var after = before | ObjectToggleConfig.ComponentEnabled;
 
 			ValidateComponentToggleStateChange(before, after);
 		}
@@ -325,8 +157,8 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void EnableComponent_OneBasicManagedFixedUpdate_AddsToSystem()
 		{
-			var before = MockObjectToggleConfig.GameObjectActive | MockObjectToggleConfig.FixedUpdate;
-			var after = before | MockObjectToggleConfig.ComponentEnabled;
+			var before = ObjectToggleConfig.GameObjectActive | ObjectToggleConfig.FixedUpdate;
+			var after = before | ObjectToggleConfig.ComponentEnabled;
 
 			ValidateComponentToggleStateChange(before, after);
 		}
@@ -334,8 +166,8 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		[Test]
 		public void EnableComponent_OneBasicManagedUpdateAndFixed_AddsBothToSystem()
 		{
-			var before = MockObjectToggleConfig.GameObjectActive | MockObjectToggleConfig.Update | MockObjectToggleConfig.FixedUpdate;
-			var after = before | MockObjectToggleConfig.ComponentEnabled;
+			var before = ObjectToggleConfig.GameObjectActive | ObjectToggleConfig.Update | ObjectToggleConfig.FixedUpdate;
+			var after = before | ObjectToggleConfig.ComponentEnabled;
 
 			ValidateComponentToggleStateChange(before, after);
 		}
@@ -347,10 +179,10 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		/// <param name="initialConfiguration"></param>
 		/// <param name="finalConfiguration"></param>
 		private void ValidateComponentToggleStateChange(
-			MockObjectToggleConfig initialConfiguration,
-			MockObjectToggleConfig finalConfiguration)
+			ObjectToggleConfig initialConfiguration,
+			ObjectToggleConfig finalConfiguration)
 		{
-			using (MockEnvironment env = new MockEnvironment(DEFAULT_GROUP_NAME))
+			using (FakeEnvironment env = new FakeEnvironment(DEFAULT_GROUP_NAME))
 			{
 				ValidateComponentToggleStateChange(
 					env,
@@ -369,19 +201,19 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		/// <param name="initialConfiguration">Configuration of instantiated component/GameObject.</param>
 		/// <param name="finalConfiguration">Final configuration of the component/GameObject.</param>
 		private void ValidateComponentToggleStateChange(
-			MockEnvironment env,
+			FakeEnvironment env,
 			string groupName,
-			MockObjectToggleConfig initialConfiguration,
-			MockObjectToggleConfig finalConfiguration)
+			ObjectToggleConfig initialConfiguration,
+			ObjectToggleConfig finalConfiguration)
 		{
 			int expectedInitialUpdateCount = (
 				env.GetCountForGroup(groupName, UpdateType.Normal) +
-				(initialConfiguration.HasFlag(MockObjectToggleConfig.UpdateActiveAndEnabled) ? 1 : 0)
+				(initialConfiguration.HasFlag(ObjectToggleConfig.UpdateActiveAndEnabled) ? 1 : 0)
 			);
 
 			int expectedInitialFixedCount = (
 				env.GetCountForGroup(groupName, UpdateType.Fixed) +
-				(initialConfiguration.HasFlag(MockObjectToggleConfig.FixedUpdateActiveAndEnabled) ? 1 : 0)
+				(initialConfiguration.HasFlag(ObjectToggleConfig.FixedUpdateActiveAndEnabled) ? 1 : 0)
 			);
 
 			env.InstantiateManagedComponents<TestBasicManagedUpdatesComponent>(
@@ -401,7 +233,7 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 
 			env.SetToggleConfigForManagedComponent(components[0], finalConfiguration);
 
-			MockEnvironment.GetExpectedDeltaForComponentToggleChange(
+			FakeEnvironment.GetExpectedDeltaForComponentToggleChange(
 				initialConfiguration,
 				finalConfiguration,
 				out int updateDelta,
@@ -417,7 +249,7 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		}
 
 		private static void AssertGroupCountForTypeEquals(
-			MockEnvironment env,
+			FakeEnvironment env,
 			string groupName,
 			UpdateType updateType,
 			int expectedCount)
@@ -436,11 +268,11 @@ namespace Miscreant.Utilities.Lifecycle.RuntimeTests
 		public IEnumerator SelfDestruct_ManagedUpdateOnlyOneInSystem()
 		{
 			string groupName = DEFAULT_GROUP_NAME;
-			using (MockEnvironment env = new MockEnvironment(groupName))
+			using (FakeEnvironment env = new FakeEnvironment(groupName))
 			{
 				env.InstantiateManagedComponents<TestManagedUpdatesSelfDestruct>(
 					groupName,
-					MockObjectToggleConfig.UpdateActiveAndEnabled
+					ObjectToggleConfig.UpdateActiveAndEnabled
 				);
 
 				AssertGroupCountForTypeEquals(env, groupName, UpdateType.Normal, 1);
