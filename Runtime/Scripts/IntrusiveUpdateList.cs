bee69c17127675e49a0f1b6b3e99325ca7b901c0
Mark Agne <magnenator@gmail.com>
2019-12-31 02:21:51 -0600

    removed unused using direcrives from IntrusiveList subclasses

--- Runtime/Scripts/IntrusiveUpdateList.cs
+++ Runtime/Scripts/IntrusiveUpdateList.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Miscreant.Lifecycle
+﻿namespace Miscreant.Lifecycle
 {
 	internal sealed class IntrusiveUpdateList : IntrusiveList
 	{
