44420270769993a2d65371483c1b793e11719042
Mark Agne <magnenator@gmail.com>
2020-01-06 23:20:20 -0600

    update config drawer is part of the correct Editor namespace

--- Editor/Scripts/CustomUpdateConfigDrawer.cs
+++ Editor/Scripts/CustomUpdateConfigDrawer.cs
@@ -1,76 +1,77 @@
 ﻿using UnityEngine;
 using UnityEditor;
 
-using Miscreant.Lifecycle;
-
-[CustomPropertyDrawer(typeof(CustomUpdateBehaviour.Config))]
-public class CustomUpdateConfigDrawer : PropertyDrawer
+namespace Miscreant.Lifecycle.Editor
 {
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+	[CustomPropertyDrawer(typeof(CustomUpdateBehaviour.Config))]
+	public class CustomUpdateConfigDrawer : PropertyDrawer
 	{
-		EditorGUI.BeginProperty(position, label, property);
-
-		bool updateFlagBefore = property.FindPropertyRelative("_update").boolValue;
-		bool fixedFlagBefore = property.FindPropertyRelative("_fixedUpdate").boolValue;
-
-		EditorGUI.BeginChangeCheck();
-
-		EditorGUI.PropertyField(position, property, label, property.isExpanded);
-
-		if (EditorGUI.EndChangeCheck() && EditorApplication.isPlaying)
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
-			property.serializedObject.ApplyModifiedProperties();
+			EditorGUI.BeginProperty(position, label, property);
 
-			bool updateFlagAfter = property.FindPropertyRelative("_update").boolValue;
-			bool fixedFlagAfter = property.FindPropertyRelative("_fixedUpdate").boolValue;
+			bool updateFlagBefore = property.FindPropertyRelative("_update").boolValue;
+			bool fixedFlagBefore = property.FindPropertyRelative("_fixedUpdate").boolValue;
 
-			bool didChange = (
-				(updateFlagBefore != updateFlagAfter) ||
-				(fixedFlagBefore != fixedFlagAfter)
-			);
+			EditorGUI.BeginChangeCheck();
 
-			if (didChange)
+			EditorGUI.PropertyField(position, property, label, property.isExpanded);
+
+			if (EditorGUI.EndChangeCheck() && EditorApplication.isPlaying)
 			{
-				bool becameActive = (
-					(!updateFlagBefore && updateFlagAfter) ||
-					(!fixedFlagBefore && fixedFlagAfter)
+				property.serializedObject.ApplyModifiedProperties();
+
+				bool updateFlagAfter = property.FindPropertyRelative("_update").boolValue;
+				bool fixedFlagAfter = property.FindPropertyRelative("_fixedUpdate").boolValue;
+
+				bool didChange = (
+					(updateFlagBefore != updateFlagAfter) ||
+					(fixedFlagBefore != fixedFlagAfter)
 				);
 
-				var selectedObjects = property.serializedObject.targetObjects;
-				System.Array.Sort(selectedObjects, new InstanceIdComparer());
-
-				foreach (var selection in selectedObjects)
+				if (didChange)
 				{
-					var updateBehaviour = selection as CustomUpdateBehaviour;
-					if (updateBehaviour != null)
+					bool becameActive = (
+						(!updateFlagBefore && updateFlagAfter) ||
+						(!fixedFlagBefore && fixedFlagAfter)
+					);
+
+					var selectedObjects = property.serializedObject.targetObjects;
+					System.Array.Sort(selectedObjects, new InstanceIdComparer());
+
+					foreach (var selection in selectedObjects)
 					{
-						updateBehaviour.updateConfig.valueChangedAction?.Invoke(becameActive);
+						var updateBehaviour = selection as CustomUpdateBehaviour;
+						if (updateBehaviour != null)
+						{
+							updateBehaviour.updateConfig.valueChangedAction?.Invoke(becameActive);
+						}
 					}
 				}
 			}
+
+			EditorGUI.EndProperty();
 		}
 
-		EditorGUI.EndProperty();
-	}
-
-	public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-	{
-		return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
-	}
-
-	private class InstanceIdComparer : System.Collections.Generic.IComparer<Object>
-	{
-		public int Compare(Object one, Object other)
+		public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
 		{
-			if (one.GetInstanceID() < other.GetInstanceID())
-			{
-				return 1;
-			}
-			else if (one.GetInstanceID() > other.GetInstanceID())
-			{
-				return -1;
-			}
-			return 0;
+			return EditorGUI.GetPropertyHeight(property, label, property.isExpanded);
 		}
-	}
+
+		private class InstanceIdComparer : System.Collections.Generic.IComparer<Object>
+		{
+			public int Compare(Object one, Object other)
+			{
+				if (one.GetInstanceID() < other.GetInstanceID())
+				{
+					return 1;
+				}
+				else if (one.GetInstanceID() > other.GetInstanceID())
+				{
+					return -1;
+				}
+				return 0;
+			}
+		}
+	}	
 }
